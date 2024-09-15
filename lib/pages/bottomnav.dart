9458b1d6868299acdfb76eb3f80d124db5d901d1
lyltn <lyltn.21it@vku.udn.vn>
2024-09-15 20:23:09 +0700

    fist commit

---
+++ lib/pages/bottomnav.dart
@@ -0,0 +1,65 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:ecommercettl/pages/home.dart';
+import 'package:ecommercettl/pages/order.dart';
+import 'package:ecommercettl/pages/post.dart';
+import 'package:ecommercettl/pages/profile.dart';
+import 'package:flutter/material.dart';
+
+class BottomNav extends StatefulWidget {
+  const BottomNav({super.key});
+
+  @override
+  State<BottomNav> createState() => _BottomNavState();
+}
+
+class _BottomNavState extends State<BottomNav> {
+  int currentTabIndex = 0;
+
+  late List<Widget> pages;
+  late Widget currentPage;
+  late Home homepage;
+  late Profile profile;
+  late Post post;
+  late Order order;
+
+  @override
+  void initState() {
+    homepage = Home();
+    order = Order();
+    profile = Profile();
+    post = Post();
+    pages = [homepage, post, order, profile];
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: Column(
+        mainAxisSize: MainAxisSize.min, // Đảm bảo kích thước cột tối thiểu
+        children: [
+          CurvedNavigationBar(
+            height: 50,
+            backgroundColor: Colors.white,
+            color: Color(0xFF15A362),
+            animationDuration:
+                Duration(milliseconds: 500), // Đổi thành milliseconds
+            onTap: (int index) {
+              setState(() {
+                currentTabIndex = index;
+              });
+            },
+            items: [
+              Icon(Icons.home_outlined, color: Colors.white),
+              Icon(Icons.article_outlined, color: Colors.white),
+              Icon(Icons.local_shipping_outlined, color: Colors.white),
+              Icon(Icons.person_outline, color: Colors.white),
+            ],
+          ),
+          // Phần tên tab nằm dưới biểu tượng của thanh điều hướng
+        ],
+      ),
+      body: pages[currentTabIndex],
+    );
+  }
+}
---
+++ lib/pages/home.dart
@@ -0,0 +1,44 @@
+import 'package:ecommercettl/widget/widget_support.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.only(top: 50.0, left: 20.0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "hello lyly",
+                  style: AppWiget.boldTextFeildStyle(),
+                ),
+                Container(
+                  margin: EdgeInsets.only(right: 20.0),
+                  padding: EdgeInsets.all(3),
+                  decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(8)),
+                  child: Icon(
+                    Icons.shopping_cart,
+                    color: Colors.white,
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
