365e9fd7b43c78f05a47ab21537703349a74301c
zungcao1004 <dungcq.100403@gmail.com>
2024-10-21 19:00:35 +0700

    add guarantee

--- lib/main.dart
+++ lib/main.dart
@@ -52,7 +52,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const BottomNav(),
+      home: const ProductListPage(),
       // home: const ProductListPage(),
     );
   }
