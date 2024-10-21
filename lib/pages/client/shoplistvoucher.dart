f669a78e6541810e12b09ac46d4258a45e4c18c3
lyltn <lyltn.21it@vku.udn.vn>
2024-10-21 18:20:06 +0700

    ordercus

---
+++ lib/pages/client/shoplistvoucher.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShopListVoucher extends StatefulWidget {
+  const ShopListVoucher({super.key});
+
+  @override
+  State<ShopListVoucher> createState() => _ShopListVoucherState();
+}
+
+class _ShopListVoucherState extends State<ShopListVoucher> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
