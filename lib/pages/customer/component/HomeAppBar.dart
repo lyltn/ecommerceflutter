1864d36d0748b583734e5c3c7540d99393e98d3d
truongkien03 <hello2k3.2k3@gmail.com>
2024-10-22 10:26:55 +0700

    update loading login and profile user

--- lib/pages/customer/component/HomeAppBar.dart
+++ lib/pages/customer/component/HomeAppBar.dart
@@ -12,12 +12,12 @@ class HomeAppBar extends StatelessWidget {
       title: Row(
         mainAxisAlignment: MainAxisAlignment.center, // Căn giữa hàng
         children: [
-          Image.asset(
-            'images/logo-cus.png', // Đường dẫn tới hình ảnh
-            width: 50,
-            height: 50,
-            fit: BoxFit.cover,
-          ),
+          // Image.asset(
+          //   'images/logo-cus.png', // Đường dẫn tới hình ảnh
+          //   width: 50,
+          //   height: 50,
+          //   fit: BoxFit.cover,
+          // ),
           SizedBox(width: 8),
           Text(
             'TTL',
